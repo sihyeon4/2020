195fbbaeeef900a34c7dc7d9cc31c70b67c05ed2
sihyeon4 <67040669+sihyeon4@users.noreply.github.com>
2020-06-17 15:32:29 +0900

    Initial commit

---
+++ printf1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+	/*int mile;
+	printf("마일을 입력하세요 : ");
+	scanf("%d", &mile);
+	printf("%d마일은 %d 미터입니다.", mile, 1609 * mile);*/
+    /*int bottom, height;
+	printf("삼각형의 밑변 : ");
+	scanf("%d", &bottom);
+	printf("삼각형의 높이 : ");
+	scanf("%d", &height);
+	printf("삼각형의 넓이 : %.2f", (bottom * height) / 2.0);*/
+	/*int F;
+	printf("화씨값을 입력하시오 : ");
+	scanf("%d", &F);
+	printf("%.2f", 5 / 9.0*(F - 32));*/
+	/*float x;
+	printf("실수를 입력하시오 : ");
+	scanf("%f", &x);
+	printf("다항식의 값은 : %", 3 * (x * x) + 7 * x + 11);*/
+	/*int a;
+	printf("16진수 정수를 입력하시오 : ");
+	scanf("%x", &a);
+	printf("%o\n", a);
+	printf("%d\n", a);
+	printf("%x\n", a);*/
+	/*int x = 10, y = 20, temp;
+	printf("x = %d y = %d\n", x, y);
+	temp = x;
+	x = y;
+	y = temp;
+	printf("x = %d y = %d\n", x, y);*/
+	/*double a, b, c;
+	printf("가로, 세로, 높이를 한 번에 입력 : ");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	printf("상자의 부피는 %lf입니다.", a * b * c);*/
+	/*double a, b, c;
+	printf("질량 : ");
+	scanf("%lf", &a);
+	printf("속도 : ");
+	scanf("%lf", &b);
+	printf("운동에너지 : %lf", (a*(b * b)) / 2.0);*/
+	//printf("%c", 'a' + 1);
+	//printf("%c", 'a' + 2);
+	//printf("%c", 'a' + 3); //문자들도 정수형으로 저장된다는 것을 알 수 있다.
+}
